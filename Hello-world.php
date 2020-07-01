6a3ed0d9dfcd3ffe1cc235e6a26e7e197b6156ec
nathanportier <nportiern@gmail.com>
2020-07-01 15:18:57 +0200

    Create Hello-world.php

---
+++ Hello-world.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Hello World</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+
+
+
+</body>
+</html>
