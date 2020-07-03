000dfea59ca4763b8a97d8cafe31280644f0c038
nathanportier <nportiern@gmail.com>
2020-07-03 14:32:34 +0200

    Create vitrine-accueil.php

---
+++ EXO 02/vitrine-accueil.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Accueil</title>
+ <meta charset="UTF-8">
+ <link rel="stylesheet" href="Style/vitrine.css">
+ </head>
+ 
+<body>
+ <header>
+ <nav>
+ <a href="vitrine-accueil.php">ACCUEIL</a>
+ <a href="vitrine-programme.php">PROGRAMME</a>
+ <a href="vitrine-contact.php">CONTACT</a>
+ </nav>
+ <h1>Summer Code Camp</h1>
+ 
+ </header>
+ 
+<main class="ACCUEIL">
+ 
+ <h2>ACCUEIL</h2>
+ 
+ <p><i>numérique, nouvelle ère de l’humanité, source d’innovation et de performance ?</i>
+ <br> Pour mieux comprendre cette révolution, venez apprendre les rudiments du code et
+ découvrir les bases de la programmation des sites internet. Destiné aux étudiants,
+ ce code camp vous initiera en un mois aux principaux langages de programmation
+ Web (HTML-CSS et PHP), vous dévoilera le fonctionnement d’outils comme <b>Google</b> ou
+ <b>Paypal</b> et vous éclaira sur la sécurité des sites internet.</p>
+ 
+ <div>
+ <img src=codage.jpeg alt="photo codage" width="300" height="150"> <img src=stagecodage.jpg alt="photo stage codage" width="300" height="150">
+ </div>
+
+
+ 
+</main>
+ 
+<footer>
+ <a href="http://www.epitech.eu"><img src=logo_epitech.png></a>
+ 
+</footer>
+</body>
+</html>
