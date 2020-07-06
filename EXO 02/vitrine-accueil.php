7bbcd87547e100a4d8a4f52d47f6fae93fde8d95
nathanportier <nportiern@gmail.com>
2020-07-06 15:50:10 +0200

    Update vitrine-accueil.php

--- EXO 02/vitrine-accueil.php
+++ EXO 02/vitrine-accueil.php
@@ -1,44 +1,28 @@
-<!DOCTYPE html>
-<html>
- <head>
- <title>Accueil</title>
- <meta charset="UTF-8">
- <link rel="stylesheet" href="Style/vitrine.css">
- </head>
- 
+  
+<html lang="fr">
+<head>
+    <title>Vitrine Summer Code Camp</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/vitrine.css">
+</head>
+
 <body>
- <header>
- <nav>
- <a href="vitrine-accueil.php">ACCUEIL</a>
- <a href="vitrine-programme.php">PROGRAMME</a>
- <a href="vitrine-contact.php">CONTACT</a>
- </nav>
- <h1>Summer Code Camp</h1>
- 
- </header>
- 
-<main class="ACCUEIL">
- 
- <h2>ACCUEIL</h2>
- 
- <p><i>numérique, nouvelle ère de l’humanité, source d’innovation et de performance ?</i>
- <br> Pour mieux comprendre cette révolution, venez apprendre les rudiments du code et
- découvrir les bases de la programmation des sites internet. Destiné aux étudiants,
- ce code camp vous initiera en un mois aux principaux langages de programmation
- Web (HTML-CSS et PHP), vous dévoilera le fonctionnement d’outils comme <b>Google</b> ou
- <b>Paypal</b> et vous éclaira sur la sécurité des sites internet.</p>
- 
- <div>
- <img src=codage.jpeg alt="photo codage" width="300" height="150"> <img src=stagecodage.jpg alt="photo stage codage" width="300" height="150">
- </div>
-
-
- 
-</main>
- 
-<footer>
- <a href="http://www.epitech.eu"><img src=logo_epitech.png></a>
- 
-</footer>
-</body>
+    <?php
+    $page_title = "Accueil";
+    include ("header.php")
+    ?>
+        <main>
+            <h2>Accueil</h2>
+            <div class="container">
+                
+                <p>Le numérique, nouvelle ère de l’humanité, source d’innovation et de performance ? Pour mieux comprendre cette révolution, venez apprendre les rudiments du code et découvrir les bases de la programmation des sites internet. Destiné aux étudiants, ce code camp vous initiera en un mois aux principaux langages de programmation Web (HTML-CSS et PHP), vous dévoilera le fonctionnement d’outils comme Google ou Paypal et vous éclaira sur la sécurité des sites internet.</p>
+            </div>
+            <div class="photo">
+                <img src="code.jpg" height="200px" width="300px">
+                <img src="net.jpg" height="200px" width="300px">
+            </div>
+        </main>
+        <?php include ("footer.php") ?>
+    </body>
 </html>
