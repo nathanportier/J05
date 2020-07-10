2899c3db91b81ad7e6408fc8e15b91175c7ccc9d
nathanportier <nportiern@gmail.com>
2020-07-10 20:00:41 +0200

    Update vitrine-accueil.php

--- EXO 02/vitrine-accueil.php
+++ EXO 02/vitrine-accueil.php
@@ -4,7 +4,7 @@
     <title>Vitrine Summer Code Camp</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/vitrine.css">
+    <link rel="stylesheet" href="../J04/Style/vitrine.css">
 </head>
 
 <body>
